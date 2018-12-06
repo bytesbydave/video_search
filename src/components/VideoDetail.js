2b1dc8995f9f90ae2fd33f6a27197b7c1b831677
Dave <dave.lin21@gmail.com>
2018-12-06 18:07:07 -0500

    Social Media links

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './VideoDetail.css';
 
+function facebook(url) {
+  return `https://www.facebook.com/sharer/sharer.php?u=${url}`
+}
+
+function twitter(url) {
+  return `https://twitter.com/home?status=${url}`
+}
+
 const VideoDetail = ({ video }) => {
   if (!video) {
     return (
@@ -8,6 +16,7 @@ const VideoDetail = ({ video }) => {
     );
   }
 
+  const videoShare = `https://youtu.be/${video.id.videoId}`
   const videoSRC = `https://www.youtube.com/embed/${video.id.videoId}`
 
   return (
@@ -18,6 +27,20 @@ const VideoDetail = ({ video }) => {
       <div className="ui segment detail-contain">
         <h4>{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
+        <div>
+          <a href={facebook(videoShare)}>
+            <button className="ui facebook button">
+              <i className="facebook icon"></i>
+              Facebook
+            </button>
+          </a>
+          <a href={twitter(videoShare)} >
+            <button className="ui twitter button">
+              <i className="twitter icon"></i>
+              Twitter
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
