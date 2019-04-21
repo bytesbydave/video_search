0a93de0d0965f02d31fa6813ec98ba2030e6cf85
Dave <dave.lin21@gmail.com>
2019-04-21 01:15:11 -0400

    refactor redux state and dispatch to respective components

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 import './App.css';
-import { requestVideos, setSearchField, selectVideo } from '../actions';
 
 class App extends React.Component {
-  componentDidMount() {
-    this.props.onTermSubmit('coffee');
-  }
-
   render() {
-    const {
-      searchField,
-      onSearchChange, onTermSubmit,
-      videos,
-      onVideoSelect,
-      selectedVideo
-    } = this.props;
     return (
       <div className="ui container top-contain">
-        <SearchBar
-          onTermSubmit={onTermSubmit}
-          onSearchChange={onSearchChange}
-          searchField={searchField}
-        />
+        <SearchBar />
         <div className="ui stackable grid">
           <div className="ui row">
             <div className="ten wide column">
-              <VideoDetail video={selectedVideo} />
+              <VideoDetail />
             </div>
             <div className="six wide column">
-              <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+              <VideoList />
             </div>
           </div>
         </div>
@@ -41,25 +24,4 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    videos: state.requestVideos.videos,
-    isPending: state.requestVideos.isPending,
-    error: state.requestVideos.error,
-    searchField: state.searchVideos.searchField,
-    selectedVideo: state.selectVideo.selectedVideo
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onTermSubmit: event => dispatch(requestVideos(event)),
-    onSearchChange: event => dispatch(setSearchField(event.target.value)),
-    onVideoSelect: event => dispatch(selectVideo(event))
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
