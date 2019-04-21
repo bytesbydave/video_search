ddcfe5cccc29a7c35213ed392b94329569bc820d
Dave <dave.lin21@gmail.com>
2019-04-21 00:27:54 -0400

    add select video action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,27 +2,38 @@ import {
   CHANGE_SEARCH_FIELD,
   REQUEST_VIDEOS_PENDING,
   REQUEST_VIDEOS_SUCCESS,
-  REQUEST_VIDEOS_FAILED
+  REQUEST_VIDEOS_FAILED,
+  SELECT_VIDEO
 } from './types';
 import youtube from '../api/youtube';
 
-export const setSearchField = (text) => {
+export const setSearchField = text => {
   return {
     type: CHANGE_SEARCH_FIELD,
     payload: text
-  }
-}
+  };
+};
 
-export const requestVideos = (term) => async dispatch => {
-  dispatch({ type: REQUEST_VIDEOS_PENDING })
+export const requestVideos = term => async dispatch => {
+  dispatch({ type: REQUEST_VIDEOS_PENDING });
   try {
     const response = await youtube.get('/search', {
       params: {
-        q: term
+        q: term,
+        type: 'video'
       }
     });
-    dispatch({ type: REQUEST_VIDEOS_SUCCESS, payload: response.data })
+    const videos = response.data.items;
+    dispatch({ type: REQUEST_VIDEOS_SUCCESS, payload: videos });
+    dispatch({ type: SELECT_VIDEO, payload: videos[0] });
   } catch (err) {
-    dispatch({ type: REQUEST_VIDEOS_FAILED, payload: err})
+    dispatch({ type: REQUEST_VIDEOS_FAILED, payload: err });
   }
-}
+};
+
+export const selectVideo = video => {
+  return {
+    type: SELECT_VIDEO,
+    payload: video
+  };
+};
