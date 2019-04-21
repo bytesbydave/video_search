d8ba40168129049177ad485696ef5089d7cf522a
Dave <dave.lin21@gmail.com>
2019-04-21 00:30:09 -0400

    refactor to incorporate redux

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-  state = { term: '' };
+  // state = { term: '' };
 
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
-  };
+  // onInputChange = (event) => {
+  //   this.setState({ term: event.target.value });
+  // };
 
-  onFormSubmit = (event) => {
-    event.preventDefault();
-    this.props.onFormSubmit(this.state.term);
-  };
+  // onFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   this.props.onFormSubmit(this.state.term);
+  // };
 
   render() {
+    const { searchField, onSearchChange, onTermSubmit } = this.props;
     return (
       <div className="search-bar ui segment search-bar-contain">
-        <form className="ui form" onSubmit={this.onFormSubmit} >
+        <div className="ui form">
           <div className="field">
-            <input 
+            <input
               placeholder="Search"
               className="input-search"
-              type="text" 
-              value={this.state.term}
-              onChange={this.onInputChange}
+              type="text"
+              value={searchField}
+              onChange={onSearchChange}
             />
             <div className="search-contain">
-              <button className="ui primary button search-btn">Search</button>
+              <button
+                onClick={() => onTermSubmit(searchField)}
+                value={searchField}
+                className="ui primary button search-btn"
+              >
+                Search
+              </button>
             </div>
           </div>
-        </form>
+        </div>
       </div>
     );
   }
