cbd8ad6b17fe172337fdb4de5088a4ba520fa48b
Dave <dave.lin21@gmail.com>
2019-04-21 00:27:20 -0400

    import redux middlewares and providers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { searchVideos, requestVideos, selectVideo } from './reducers';
 import App from './components/App';
 
+const rootReducer = combineReducers({
+  searchVideos,
+  requestVideos,
+  selectVideo
+});
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware)
+);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
