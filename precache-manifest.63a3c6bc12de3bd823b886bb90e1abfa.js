70c751e47b6234ead976c7934266aa5a1770bebb
Dave <dave.lin21@gmail.com>
2019-07-16 22:21:55 -0400

    Updates

---
+++ precache-manifest.63a3c6bc12de3bd823b886bb90e1abfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbcc6a4df89dabe9a8f0",
+    "url": "/video_search/static/css/main.7d379861.chunk.css"
+  },
+  {
+    "revision": "dbcc6a4df89dabe9a8f0",
+    "url": "/video_search/static/js/main.3fdac15b.chunk.js"
+  },
+  {
+    "revision": "27034129dafbd80d2a68",
+    "url": "/video_search/static/js/runtime~main.b23aa1c6.js"
+  },
+  {
+    "revision": "aa66017b91f05a93e6ac",
+    "url": "/video_search/static/js/2.315ca042.chunk.js"
+  },
+  {
+    "revision": "95ac867dd8589da19b512d4ffe77bf56",
+    "url": "/video_search/index.html"
+  }
+];
